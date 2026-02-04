09013dd7199ab619aa539d3429d52ccf8b1b5480
Andy Vandijck <andyvand@gmail.com>
2026-02-04 13:33:49 +0100

    Fix Windows CE debug build
    Fix Windows CE debug build

--- src/Preferences/DllMain.c
+++ src/Preferences/DllMain.c
@@ -3,25 +3,9 @@
 
 BOOL __stdcall DllMain(HANDLE hDLL, DWORD dwReason, LPVOID lpReserved)
 {
-	BOOL	bRet;
-
-	/* Make Lint Happy */
-	switch (dwReason)
-	{
-	case DLL_PROCESS_ATTACH:
-#ifdef	DEBUG
-		DEBUGREGISTER(hDLL);
-#endif
-		bRet = DisableThreadLibraryCalls((HMODULE)hDLL);
-		break;
-	case DLL_PROCESS_DETACH:
-		break;
-	default:
-		break;
-	}
-
-	dwReason = dwReason;
-	lpReserved = lpReserved;
+	(void)hDLL;
+	(void)dwReason;
+	(void)lpReserved;
 
 	return (TRUE);
 }
